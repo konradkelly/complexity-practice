0f02cf5e01d90dc5d7ffe96c3de4a561561d9b3f
konradkelly <konradky@gmail.com>
2025-10-20 20:25:04 -0700

    add answer for problem seven

--- src/Main.java
+++ src/Main.java
@@ -71,7 +71,7 @@ public class Main {
 
   // Assume that the largest number is no bigger than the length
   // of the array
-  // YOUR ANSWER HERE
+  // O(n^2) where n = the length of the nums array
   public static void computeAllFactorials(int[] nums) {
     for(int num : nums) {
         int result = computeFactorial(num);
